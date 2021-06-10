8c4371b73a77a5d29e076a4f4051ec6ee5c85976
Yi Kuo <kuokuoyiyi@gmail.com>
2021-06-10 15:36:09 +0800

    Change title and header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -121,14 +121,6 @@ const Header = ({
                       <Link to="#0" onClick={closeMenu}>團隊介紹</Link>
                     </li>
                   </ul>
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
-                      <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
-                      </li>
-                    </ul>}
                 </div>
               </nav>
             </>}
